18bb5d63ddd09b167e4cf0e86e168f983554f5b0
VickriFahrurozi <VickriFahrurozi@gmail.com>
2022-07-22 20:26:57 +0700

    feat : fix conflict

--- model/auth.js
+++ model/auth.js
@@ -132,182 +132,28 @@ module.exports = {
 		});
 	},
 
-<<<<<<< HEAD
-	forgot: (req,res) => {
-		return new Promise((resolve,reject) =>{
-			const {email} = req.body
+	forgot: (req, res) => {
+		return new Promise((resolve, reject) => {
+			const { email } = req.body;
 			const sqlcheckemail = `SELECT  * from account where account_email = '${email.toLowerCase()}'`;
-			db.query(sqlcheckemail,(err,result) =>{
+			db.query(sqlcheckemail, (err, result) => {
 				if (err) {
 					reject({
 						message: `Email not Registered , Use Another One`,
 					});
-				}
-				else if(result[0] == undefined){
+				} else if (result[0] == undefined) {
 					reject({
 						message: `Email not Registered , Use Another One`,
 					});
-				}
-				else{
-					console.log(result[0].account_email)
-					const secret = process.env.JWT_SECRET_KEY + result[0].account_password
+				} else {
+					console.log(result[0].account_email);
+					const secret =
+						process.env.JWT_SECRET_KEY + result[0].account_password;
 					const payload = {
 						id: result[0].account_id,
-						email: result[0].account_email
-					}
-					const tokenEmail = jwt.sign(payload, secret, {expiresIn: '15m'})
-
-
-					const transporter = nodemailer.createTransport({
-						service : 'gmail',
-						auth:{
-							user: 'zakiteachme12@gmail.com',
-							pass: 'cxjdjyepshqahvjs'
-						},
-						tls:{
-							rejectUnauthorized:false
-						}
-					})
-	
-					const mailOptions ={
-					from: '"Verify your email" <zakiteachme12@gmail.com>',
-					to: '12kevinsanjaya@gmail.com',
-					subject: 'dailynews -verify your email',
-					html: `<h2>${email}! Please Verify Mail for RESET PASSWORD</h2>
-							<h4>please verify your mail to continue...</h4>
-							
-	
-							<a href="http://localhost:3289/api/v1/auth/reset-password/${result[0].account_id}/token=${tokenEmail}">reset password</a>
-							`    
-					}
-	
-					transporter.sendMail(mailOptions, function(err,info){
-						if(err){
-							console.log(err)
-						}else{
-							console.log('verification email is sent to your gmail account')
-						}
-					}) 
-	
-
-				}
-			})
-		})
-	},
-	
-	getreset: (req, res) => {
-		return new Promise((resolve, reject)=> {
-			const {id,token} = req.params
-			const password = req.body.password
-			console.log(req.params)
-			const sqlCheckId = `SELECT * from account where account_id = '${id.toLowerCase()}'`;
-			db.query(sqlCheckId,(err, results)=> {
-				if(err) {
-					res.send({message: "ada error"})
-				}else if(results[0] == undefined) {
-					res.send({
-						message: 'invalid users'
-					})
-				}
-				const secret = process.env.JWT_SECRET_KEY + results[0].account_password
-
-
-			resolve({
-				token: token,
-				uye: 'uy',
-				data: results[0].account_id
-			})
-
-			})
-		  })
-	},
-	reset: (req, res) => {
-		return new Promise((resolve, reject)=> {
-			const {id,token} = req.params
-			const {password, confirmPassword}  = req.body
-			console.log(req.params)
-			const sqlCheckId = `SELECT * from account where account_id = '${id.toLowerCase()}'`;
-			db.query(sqlCheckId,(err, results)=> {
-				if(err) {
-					res.send({message: "ada error"})
-				}else if(results[0] == undefined) {
-					res.send({
-						message: 'invalid users'
-					})
-				}
-
-				const secret = process.env.JWT_SECRET_KEY + results[0].account_password
-				const payload = jwt.verify(token, secret)
-				console.log(password)
-				console.log(confirmPassword)
-				
-				if(password !== confirmPassword){
-					res.send('password and confirm password are not same')
-				}
-				console.log(password, 'ini password')
-				bcrypt.hash(password,10, function(err,hashedPassword){
-					if(err){
-						console.log(err)
-					}else{
-						console.log(hashedPassword,'ini hasil hash')
-						const previousData = {
-							...results[0],
-							...req.body, account_password: hashedPassword
-						}
-						console.log(previousData, 'ini previous data')
-						const {account_email, account_password, account_role} = previousData
-						db.query( `UPDATE account SET account_email='${account_email}',account_password='${account_password}',account_role ='${account_role}'  WHERE account_id = ${id} `,(err, resultt)=> {
-								if(err) { 
-								reject(console.log(err))
-								}
-								resolve({
-								message: "update movies success",
-								status: 200,
-								data: resultt
-								})
-						})
-
-
-					}
-
-				})	
-			
-			
-			
-			})
-		  })
-	}
-
-
-=======
-	update: (req, res) => {
-		return new Promise((resolve, reject) => {
-			const { email } = req.params;
-			const password = req.body.password;
-
-			console.log(req.params);
-			const sqlcheckemail = `SELECT * from account where account_email = '${email.toLowerCase()}'`;
-			db.query(sqlcheckemail, (err, results) => {
-				if (err) {
-					res.send({ message: 'ada error' });
-				}
-
-				bcrypt.hash(password, 10, function (err, hashedPassword) {
-					console.log(hashedPassword, 'ini hasil hashed');
-					console.log(results[0], 'ini data results');
-					const previousData = {
-						...results[0],
-						...req.body,
-						password: hashedPassword,
+						email: result[0].account_email,
 					};
-
-					const tokenEmail = jwt.sign(
-						{ user_id: results[0].id, role: results[0].role },
-						process.env.JWT_SECRET_KEY,
-						{
-							expiresIn: '1d',
-						}
-					);
+					const tokenEmail = jwt.sign(payload, secret, { expiresIn: '15m' });
 
 					const transporter = nodemailer.createTransport({
 						service: 'gmail',
@@ -324,12 +170,12 @@ module.exports = {
 						from: '"Verify your email" <zakiteachme12@gmail.com>',
 						to: '12kevinsanjaya@gmail.com',
 						subject: 'dailynews -verify your email',
-						html: `<h2>${email}! Please Verify Mail for Update</h2>
-						<h4>please verify your mail to continue...</h4>
-						
-
-						<a href="http://localhost:3289/api/v1/auth/verify-email?token=${tokenEmail}">Verify your email</a>
-						`,
+						html: `<h2>${email}! Please Verify Mail for RESET PASSWORD</h2>
+							<h4>please verify your mail to continue...</h4>
+							
+	
+							<a href="http://localhost:3289/api/v1/auth/reset-password/${result[0].account_id}/token=${tokenEmail}">reset password</a>
+							`,
 					};
 
 					transporter.sendMail(mailOptions, function (err, info) {
@@ -339,28 +185,88 @@ module.exports = {
 							console.log('verification email is sent to your gmail account');
 						}
 					});
-
-					// console.log(tokenEmail, 'ini token')
-					// console.log('id', results[0].account_id)
-					// console.log('role', results[0].account_role)
-				});
-
-				//   console.log(previousData.cover)
-				//   const {title, id_categories, cover, release_date, director, description, casts} = previousData
-				//   let date = moment(release_date).format('YYYY-MM-DD')
-				//   const sql = `UPDATE movies SET title='${title}',id_categories='${id_categories}', cover='${cover}',  release_date='${date}', director='${director}',description='${description}', casts='${casts}' WHERE id = ${id} `
-				//   db.query( sql,(err, results)=> {
-				// 	if(err) {
-				// 	  reject(console.log(err))
-				// 	}
-				// 	resolve({
-				// 	  message: "update movies success",
-				// 	  status: 200,
-				// 	  data: results
-				// 	})
-				//   })
+				}
 			});
 		});
 	},
->>>>>>> 1ac8cb3f982493258f5bc6b4d473bac91ac6a811
+
+	getreset: (req, res) => {
+		return new Promise((resolve, reject) => {
+			const { id, token } = req.params;
+			const password = req.body.password;
+			console.log(req.params);
+			const sqlCheckId = `SELECT * from account where account_id = '${id.toLowerCase()}'`;
+			db.query(sqlCheckId, (err, results) => {
+				if (err) {
+					res.send({ message: 'ada error' });
+				} else if (results[0] == undefined) {
+					res.send({
+						message: 'invalid users',
+					});
+				}
+				const secret = process.env.JWT_SECRET_KEY + results[0].account_password;
+
+				resolve({
+					token: token,
+					uye: 'uy',
+					data: results[0].account_id,
+				});
+			});
+		});
+	},
+	reset: (req, res) => {
+		return new Promise((resolve, reject) => {
+			const { id, token } = req.params;
+			const { password, confirmPassword } = req.body;
+			console.log(req.params);
+			const sqlCheckId = `SELECT * from account where account_id = '${id.toLowerCase()}'`;
+			db.query(sqlCheckId, (err, results) => {
+				if (err) {
+					res.send({ message: 'ada error' });
+				} else if (results[0] == undefined) {
+					res.send({
+						message: 'invalid users',
+					});
+				}
+
+				const secret = process.env.JWT_SECRET_KEY + results[0].account_password;
+				const payload = jwt.verify(token, secret);
+				console.log(password);
+				console.log(confirmPassword);
+
+				if (password !== confirmPassword) {
+					res.send('password and confirm password are not same');
+				}
+				console.log(password, 'ini password');
+				bcrypt.hash(password, 10, function (err, hashedPassword) {
+					if (err) {
+						console.log(err);
+					} else {
+						console.log(hashedPassword, 'ini hasil hash');
+						const previousData = {
+							...results[0],
+							...req.body,
+							account_password: hashedPassword,
+						};
+						console.log(previousData, 'ini previous data');
+						const { account_email, account_password, account_role } =
+							previousData;
+						db.query(
+							`UPDATE account SET account_email='${account_email}',account_password='${account_password}',account_role ='${account_role}'  WHERE account_id = ${id} `,
+							(err, resultt) => {
+								if (err) {
+									reject(console.log(err));
+								}
+								resolve({
+									message: 'update movies success',
+									status: 200,
+									data: resultt,
+								});
+							}
+						);
+					}
+				});
+			});
+		});
+	},
 };
