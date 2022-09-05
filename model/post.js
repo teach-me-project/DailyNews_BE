fbad4cd6c9746029d1281acc0b2fb8ba11d79034
muhammadsandi12 <muhammadsandimaulana12@gmail.com>
2022-09-05 09:45:29 +0700

    feat: add search get all accepted post

--- model/post.js
+++ model/post.js
@@ -78,12 +78,11 @@ module.exports = {
 	getAllAcceptedPost: (req, res) => {
 		//get All Movies With Join
 		return new Promise((resolve, reject) => {
-			const { limit, page, order_by, sort } = req.query;
+			const { limit, page, order_by, sort, search } = req.query;
 			let offset = page * limit - limit;
-			db.query(
-				`SELECT post.post_id,post.profile_id, post.post_cover,post.post_title,post.post_category,post.post_fill, post_statistic.like_count,post_statistic.comment_count 
-				from post JOIN post_statistic on post.post_id = post_statistic.post_id
-				 where post_status = 'accepted' ORDER BY ${order_by} ${sort} limit ${limit} OFFSET ${offset} `,
+			db.query(`SELECT post.post_id,post.profile_id, post.post_cover,post.post_title,post.post_category,post.post_fill, post_statistic.like_count,post_statistic.comment_count, post.created_at 
+				from post JOIN post_statistic on post.post_id = post_statistic.post_id ${search ? `where post.post_title like search AND post_status = 'accepted' `:`where post_status = 'accepted' `} 
+				ORDER BY ${order_by} ${sort} limit ${limit} OFFSET ${offset} `,
 				(error, result) => {
 					db.query(
 						`SELECT * from post where post_status = 'accepted'`,
